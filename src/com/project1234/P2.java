2a498d0f0bea4f5e6ae4c5c9b230ab2546c89aed
archanadande <archanadande09@gmail.com>
2022-12-16 20:45:52 +0530

    add space in logical prg

---
+++ src/com/project1234/P2.java
@@ -0,0 +1,24 @@
+package com.project1234;
+//12345
+//1234
+//123
+//12
+//1
+//rows=5, num=5
+public class P2
+{
+public static void main(String[] args) 
+{
+	
+	int row=5;
+	int num=5;
+for(int i=row;i>=1;i--)
+{
+	for(int j=1;j<=i;j++) 
+	{
+		System.out.print(j);
+	}
+	System.out.println();
+	}
+}
+}
